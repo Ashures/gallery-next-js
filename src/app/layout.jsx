dc371edf713e2ff06892107c076e64a5231ee989
Ashures <ashuresyt@gmail.com>
2023-08-27 22:06:03 +1200

    Reformatted default layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,12 +1,14 @@
 export const metadata = {
-  title: 'Gallery - Ashures',
-  description: 'Gallery made by Ashures',
+    title: 'Gallery - Ashures',
+    description: 'Gallery made by Ashures',
 }
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>
+                {children}
+            </body>
+        </html>
+    );
 }
